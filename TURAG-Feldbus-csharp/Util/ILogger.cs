968eb895c2e2472d79cfe95e6b04e99b53f0566a
Martin Oemus <martin@oemus.net>
2020-07-31 18:42:02 +0200

    added ability to output log messages by supplying a message sink

---
+++ TURAG-Feldbus-csharp/Util/ILogger.cs
@@ -0,0 +1,51 @@
+﻿namespace Fraunhofer.IKTS.EddyCurrent.EdCAL.Utils
+{
+    /// <summary>
+    /// Interface for logging debug messages. Implementations
+    /// are required to be thread-safe.
+    /// </summary>
+    public interface ILogger
+    {
+        /// <summary>
+        /// Logs a verbose debug message.
+        /// </summary>
+        /// <param name="message">Verbose debug message.</param>
+        /// <param name="context">Reference to calling object.</param>
+        void Verbose(string message, object context);
+
+        /// <summary>
+        /// Logs a debug message.
+        /// </summary>
+        /// <param name="message">Debug message.</param>
+        /// <param name="context">Reference to calling object.</param>
+        void Debug(string message, object context);
+
+        /// <summary>
+        /// Logs an informational message.
+        /// </summary>
+        /// <param name="message">Informational message.</param>
+        /// <param name="context">Reference to calling object.</param>
+        void Information(string message, object context);
+
+        /// <summary>
+        /// Logs a warning.
+        /// </summary>
+        /// <param name="message">Warning to log.</param>
+        /// <param name="context">Reference to calling object.</param>
+        void Warning(string message, object context);
+
+        /// <summary>
+        /// Logs an error.
+        /// </summary>
+        /// <param name="message">Error message to log.</param>
+        /// <param name="context">Reference to calling object.</param>
+        void Error(string message, object context);
+
+        /// <summary>
+        /// Logs a fatal error.
+        /// </summary>
+        /// <param name="message">Fatal error message to log.</param>
+        /// <param name="context">Reference to calling object.</param>
+        void Fatal(string message, object context);
+    }
+}
---
+++ TURAG-Feldbus-csharp/Util/Log.cs
@@ -0,0 +1,21 @@
+﻿namespace Fraunhofer.IKTS.EddyCurrent.EdCAL.Utils
+{
+    /// <summary>
+    /// Static class representing the logger used 
+    /// to communicate debug messages. By default no log sink is set
+    /// and no messages are emitted.
+    /// </summary>
+    public static class Log
+    {
+        /// <summary>
+        /// Sets the log sink.
+        /// </summary>
+        /// <param name="logger">Log sink log messages are relayed to.</param>
+        public static void SetLogSink(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        internal static ILogger Logger { get; set; }
+    }
+}
