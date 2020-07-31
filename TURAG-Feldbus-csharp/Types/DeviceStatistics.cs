2188e79fe6058b84980326ea6ac15c0aea53e77b
Martin Oemus <martin@oemus.net>
2020-07-31 18:49:49 +0200

    refactoring

---
+++ TURAG-Feldbus-csharp/Types/DeviceStatistics.cs
@@ -0,0 +1,39 @@
+﻿namespace TURAG.Feldbus.Types
+{
+    /// <summary>
+    /// Contains information about the number of successful and failed transmissions
+    /// of the device with its bus host.
+    /// </summary>
+    public class DeviceStatistics
+    {
+        internal DeviceStatistics(uint correct, uint overflow, uint lost, uint chksum_error)
+        {
+            NoError = correct;
+            BufferOverflow = overflow;
+            LostPackets = lost;
+            ChecksumError = chksum_error;
+        }
+
+        /// <summary>
+        /// Number of successful transmissions.
+        /// </summary>
+        public uint NoError { get; }
+
+        /// <summary>
+        /// Number of packets which had to be discarded because they 
+        /// didn't fit in the input buffer.
+        /// </summary>
+        public uint BufferOverflow { get; }
+
+        /// <summary>
+        /// number of packets which were not processed when a new packet
+        /// was written to the input buffer.
+        /// </summary>
+        public uint LostPackets { get; }
+
+        /// <summary>
+        /// Number of packets which were discarded due to a checksum mismatch.
+        /// </summary>
+        public uint ChecksumError { get; }
+    }
+}
---
+++ TURAG-Feldbus-csharp/Types/HostStatistics.cs
@@ -0,0 +1,43 @@
+﻿namespace TURAG.Feldbus.Types
+{
+    /// <summary>
+    /// Contains information about the number of successful and failed transmissions
+    /// of the host with the bus device.
+    /// </summary>
+    public class HostStatistics
+    {
+        internal HostStatistics(uint checksumErrors, uint noAnswer, uint missingData, uint transmitErrors, uint noErrors)
+        {
+            ChecksumErrors = checksumErrors;
+            NoAnswer = noAnswer;
+            MissingData = missingData;
+            TransmitErrors = transmitErrors;
+            NoErrors = noErrors;
+        }
+
+        /// <summary>
+        /// Number of packets which were discarded due to a checksum mismatch.
+        /// </summary>
+        public uint ChecksumErrors { get; }
+
+        /// <summary>
+        /// Number of transmissions which were unanswered by the device.
+        /// </summary>
+        public uint NoAnswer { get; }
+
+        /// <summary>
+        /// Number of transmissions whose response was shorter than requested.
+        /// </summary>
+        public uint MissingData { get; }
+
+        /// <summary>
+        /// Number of transmissions which failed due to errors during transmission.
+        /// </summary>
+        public uint TransmitErrors { get; }
+
+        /// <summary>
+        /// Number of successful transmissions.
+        /// </summary>
+        public uint NoErrors { get; }
+    }
+}
